81a0e2382e7becb1c64853083c5e2677a0d10ed7
Sven Sporer <sohooo@intothespirit.com>
2021-11-28 12:00:04 +0100

    nvim-cmp tab completion

--- lua/sohooo/nvim-cmp.lua
+++ lua/sohooo/nvim-cmp.lua
@@ -7,21 +7,41 @@
         vim.fn["vsnip#anonymous"](args.body)
       end,
     },
+
+    -- mappings ------------------------------------------------------------
     mapping = {
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-y>'] = cmp.config.disable, --`cmp.config.disable` to remove the default `<C-y>` mapping
       ['<C-e>'] = cmp.mapping({
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      -- ['<CR>'] = cmp.mapping.confirm {
-      --   behavior = cmp.ConfirmBehavior.Replace,
-      --   select = true,
-      -- },
+    -- tab completion
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
     },
+
+    -- sources ------------------------------------------------------------
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'vsnip' },
