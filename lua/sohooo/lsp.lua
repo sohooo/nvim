5c9605c0203d72eead1d7f01ccad377be3433570
Sven Sporer <sohooo@intothespirit.com>
2022-05-11 22:11:39 +0200

    diag symbols in sign col

--- lua/sohooo/lsp.lua
+++ lua/sohooo/lsp.lua
@@ -124,10 +124,9 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 -- Change diagnostic symbols in the sign column (gutter)
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
-local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
-
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
