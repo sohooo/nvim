1a3025cd9796e61205c20a8dd8c9e591b90805cd
Sven Sporer <sohooo@intothespirit.com>
2022-10-24 23:27:56 +0200

    lsp config, lua-language-server

--- lua/sohooo/lsp.lua
+++ lua/sohooo/lsp.lua
@@ -16,13 +16,15 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
 -- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
-local servers = { 'solargraph', 'gopls', 'elixirls', 'sumneko_lua' }
+-- local servers = { 'solargraph', 'gopls', 'elixirls', 'sumneko_lua' }
+local servers = { 'solargraph', 'gopls', 'sumneko_lua' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
   }
 end
+
 
 -- rust
 require'lspconfig'.rust_analyzer.setup{}
@@ -46,52 +48,18 @@ require('lspconfig').solargraph.setup {
 
 -- elixir
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#elixirls
-require'lspconfig'.elixirls.setup{
-    cmd = { os.getenv("HOME") .. "/Code/elixir/ls/language_server.sh" };
-}
+-- require'lspconfig'.elixirls.setup{
+    -- cmd = { os.getenv("HOME") .. "/Code/elixir/ls/language_server.sh" };
+-- }
 
 -- lua
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
--- local sumneko_binary_path = vim.fn.exepath('lua-language-server')
--- local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
-
--- return file names set under path
--- it's possible to use "*", "?", which "ls" support
-local function getFiles(path)
-  local result = {}
-  local output = io.popen("ls -tr " .. path .. " | cat")
-  for line in output:lines() do
-    table.insert(result, line)
-  end
-  output:close()
-  return result
-end
-
-local function getLatest(path)
-  files = getFiles(path)
-  return files[#files]
-end
-
--- look at this dance!, just to avoid hardcoding the version number =)
-local lua_ls_path = "/opt/homebrew/Cellar/lua-language-server/"
-local lua_ls_latest = getLatest(lua_ls_path)
--- local sumneko_root_path = "/opt/homebrew/Cellar/lua-language-server/2.5.3/libexec/bin/macOS"
-local sumneko_root_path = lua_ls_path .. lua_ls_latest .. "/libexec/bin/macOS"
-local sumneko_binary_path = sumneko_root_path .. "/lua-language-server"
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 require'lspconfig'.sumneko_lua.setup {
-  cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
