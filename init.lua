712d88e4aa21446df4bf40b476793dbe61da9643
Sven Sporer <sohooo@intothespirit.com>
2022-12-11 12:33:39 +0100

    treesitter bootstrap

--- init.lua
+++ init.lua
@@ -94,10 +94,13 @@ require('packer').startup(function(use)
 	-- }}}
 
 	-- treesitter {{{
-	-- Plug 'lewis6991/spellsitter.nvim'
+  -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
 	use {
 		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
 	}
 	use 'nvim-treesitter/nvim-treesitter-textobjects'
 	use 'JoosepAlviste/nvim-ts-context-commentstring'
