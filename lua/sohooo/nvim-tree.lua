e1de9f450a8a625007134d1c21805d59eedf0e40
Sven Sporer <sohooo@intothespirit.com>
2023-04-09 17:49:42 +0200

    formatting

--- lua/sohooo/nvim-tree.lua
+++ lua/sohooo/nvim-tree.lua
@@ -1,51 +1,50 @@
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
 local list = {
   -- custom mappings
-  { key = "s", cb = tree_cb("vsplit") },
-  { key = "i", cb = tree_cb("split") },
-  { key = "t", cb = tree_cb("tabnew") },
-  { key = "?", cb = tree_cb("toggle_help") },
+  { key = 's', cb = tree_cb 'vsplit' },
+  { key = 'i', cb = tree_cb 'split' },
+  { key = 't', cb = tree_cb 'tabnew' },
+  { key = '?', cb = tree_cb 'toggle_help' },
 }
 
-
 -- following options are the default
-require'nvim-tree'.setup {
+require('nvim-tree').setup {
   -- disables netrw completely
-  disable_netrw       = true,
+  disable_netrw = true,
   -- hijack netrw window on startup
-  hijack_netrw        = true,
+  hijack_netrw = true,
   -- open the tree when running this setup function
-  open_on_setup       = false,
+  open_on_setup = false,
   -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
+  ignore_ft_on_setup = {},
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
+  open_on_tab = false,
   -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
+  hijack_cursor = false,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
+  update_cwd = false,
   -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
   update_focused_file = {
     -- enables the feature
-    enable      = false,
+    enable = false,
     -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
     -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
+    update_cwd = false,
     -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
     -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
+    ignore_list = {},
   },
   -- configuration options for the system open command (`s` in the tree by default)
   system_open = {
     -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
+    cmd = nil,
     -- the command arguments as a list
-    args = {}
+    args = {},
   },
 
   actions = {
     open_file = {
-        resize_window = true
+      resize_window = true,
     },
   },
 
@@ -65,8 +64,8 @@ require'nvim-tree'.setup {
       -- if true, it will only use your list to set the mappings
       custom_only = false,
       -- list of mappings to set on the tree manually
-      list = list
-    }
+      list = list,
+    },
   },
 
   renderer = {
@@ -74,25 +73,25 @@ require'nvim-tree'.setup {
     group_empty = false,
     highlight_git = false,
     full_name = false,
-    highlight_opened_files = "none",
-    root_folder_modifier = ":~",
+    highlight_opened_files = 'none',
+    root_folder_modifier = ':~',
     indent_width = 2,
     indent_markers = {
       enable = false,
       inline_arrows = true,
       icons = {
-        corner = "└",
-        edge = "│",
-        item = "│",
-        bottom = "─",
-        none = " ",
+        corner = '└',
+        edge = '│',
+        item = '│',
+        bottom = '─',
+        none = ' ',
       },
     },
     icons = {
       webdev_colors = true,
-      git_placement = "before",
-      padding = " ",
-      symlink_arrow = " ➛ ",
+      git_placement = 'before',
+      padding = ' ',
+      symlink_arrow = ' ➛ ',
       show = {
         file = Hifi(),
         folder = Hifi(),
@@ -100,36 +99,35 @@ require'nvim-tree'.setup {
         git = Hifi(),
       },
       glyphs = {
-        default = "",
-        symlink = "",
-        bookmark = "",
+        default = '',
+        symlink = '',
+        bookmark = '',
         folder = {
-          arrow_closed = "▸",
-          arrow_open = "▾",
+          arrow_closed = '▸',
+          arrow_open = '▾',
           -- arrow_closed = "▹",
           -- arrow_open = "▿",
           -- arrow_closed = "",
           -- arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
+          default = '',
+          open = '',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+          symlink_open = '',
         },
         git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
+          unstaged = '✗',
+          staged = '✓',
+          unmerged = '',
+          renamed = '➜',
+          untracked = '★',
+          deleted = '',
+          ignored = '◌',
         },
       },
     },
-    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    special_files = { 'Cargo.toml', 'Makefile', 'README.md', 'readme.md' },
     symlink_destination = true,
   },
 }
-
