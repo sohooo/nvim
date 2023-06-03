6c6ddaf403f8d7bc46d147c3a72ca6190f2ebb7f
Sven Sporer <sohooo@intothespirit.com>
2023-06-03 21:18:09 +0200

    fix: maps to move selected lines

--- lua/sohooo/which-key.lua
+++ lua/sohooo/which-key.lua
@@ -11,8 +11,8 @@ local telescope = require('telescope.builtin')
 wk.setup {}
 
 -- separate binding to avoid duplicate index
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv==kgvo<esc>=kgvo", { desc = "move highlighted text down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv==jgvo<esc>=jgvo", { desc = "move highlighted text up" })
 vim.keymap.set("n", "Q", "<nop>")
 
 -- lazygit terminal
