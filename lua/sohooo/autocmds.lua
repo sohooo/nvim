69d7181971a93cfe1242d3959de2e89be4daf887
Sven Sporer <sohooo@intothespirit.com>
2023-06-08 15:04:35 +0200

    chore: format on save stub

--- lua/sohooo/autocmds.lua
+++ lua/sohooo/autocmds.lua
@@ -1,30 +1,43 @@
-vim.api.nvim_create_augroup('bufcheck', {clear = true})
+vim.api.nvim_create_augroup('bufcheck', { clear = true })
+
+-- format on save
+-- FIXME: check for lsp server
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   buffer = buffer,
+--   callback = function()
+--     vim.lsp.buf.format { async = false }
+--   end,
+-- })
 
 -- reload config file on change
 vim.api.nvim_create_autocmd('BufWritePost', {
-  group    = 'bufcheck',
-  pattern  = vim.env.MYVIMRC,
-  command  = 'silent source %'})
+  group = 'bufcheck',
+  pattern = vim.env.MYVIMRC,
+  command = 'silent source %',
+})
 
 -- highlight yanks
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group    = 'bufcheck',
-  pattern  = '*',
-  callback = function() vim.highlight.on_yank{timeout=500} end })
+  group = 'bufcheck',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { timeout = 500 }
+  end,
+})
 
 -- start terminal in insert mode
 vim.api.nvim_create_autocmd('TermOpen', {
-  group    = 'bufcheck',
-  pattern  = '*',
-  command  = 'startinsert | set winfixheight'})
+  group = 'bufcheck',
+  pattern = '*',
+  command = 'startinsert | set winfixheight',
+})
 
 -- start git messages in insert mode
 vim.api.nvim_create_autocmd('FileType', {
-  group    = 'bufcheck',
-  pattern  = { 'gitcommit', 'gitrebase', },
-  command  = 'startinsert | 1'})
+  group = 'bufcheck',
+  pattern = { 'gitcommit', 'gitrebase' },
+  command = 'startinsert | 1',
+})
 
 -- Strip trailing whitespaces on save
-vim.api.nvim_create_autocmd(
-  "BufWritePre",
-  { pattern = "*", command = "%s/\\s\\+$//e" })
+vim.api.nvim_create_autocmd('BufWritePre', { pattern = '*', command = '%s/\\s\\+$//e' })
