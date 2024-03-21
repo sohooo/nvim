3bad364825b629d2e5810eb2406fdd6da528686e
Sven Sporer <sohooo@intothespirit.com>
2024-03-21 19:53:05 +0100

    fix: neotest deps

--- init.lua
+++ init.lua
@@ -168,6 +168,7 @@ require("lazy").setup({
     "nvim-neotest/neotest",
     lazy = true,
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "antoinemadec/FixCursorHold.nvim",
